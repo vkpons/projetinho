f61962f07ea66202855c273fbb21b6fe0655ec3d
vkpons <vkpons@hotmail.com>
2023-05-19 12:00:04 -0300

    correção

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect('mongobd://localhost:27017/myDataBase', {
+module.exports = mongoose.connect('mongodb://127.0.0.1:27017', {
     useNewUrlParser: true
 });
